e00b53f5b6d42ce55709531c70f66ad472ca86d5
OmegaRogue <omegarogue@users.noreply.github.com>
2019-03-25 16:49:21 +0100

    Started Button Proccessing
    Signed-off-by: OmegaRogue <philipp@buennagel.com>

--- JoyconVR/Program.cs
+++ JoyconVR/Program.cs
@@ -4,15 +4,34 @@ using System.Threading;
 using System.Threading.Tasks;
 using Joycon4CS;
 
+#pragma warning disable 4014
 namespace JoyconVR
 {
+	#region Events
+
+	public delegate string MyDel(string str);
+
+	#endregion
+
+
 	internal class Program
 	{
+		#region Vars
+
+		#region Joycon
+
 		private static JoyconManager _joyconManager;
 
-		private static readonly CancellationTokenSource CancelSource = new CancellationTokenSource();
+		#endregion
 
+		#region Cancel
+
+		private static readonly CancellationTokenSource CancelSource = new CancellationTokenSource();
 		private static readonly CancellationToken Cancel = CancelSource.Token;
+
+		#endregion
+
+		#region Vectors
 
 		private static Vector3 _lPos = new Vector3(1, -1, 0);
 		private static Vector3 _rPos = new Vector3(-1, -1, 0);
@@ -21,16 +40,49 @@ namespace JoyconVR
 		private static Vector3 _lRot = Vector3.Zero;
 		private static Vector3 _rRot = Vector3.Zero;
 
-		private static Process _cmdPro;
+		#endregion
 
+		#region CmdProcess
+
+		private static Process _cmdPro;
 		private static ProcessStartInfo _startInfo;
 
+		#endregion
+
+		#region ButtonBuffer
+
+		private static bool btnLDpadDown;
+		private static bool btnLDpadUp;
+		private static bool btnLDpadLeft;
+		private static bool btnLDpadRight;
+		private static bool btnLSL;
+		private static bool btnLSR;
+		private static bool btnLMinus;
+		private static bool btnLCapture;
+		private static bool btnLStick;
+		private static bool btnLL;
+		private static bool btnLZL;
+
+		private static bool btnRDpadDown;
+		private static bool btnRDpadUp;
+		private static bool btnRDpadLeft;
+		private static bool btnRDpadRight;
+		private static bool btnRSL;
+		private static bool btnRSR;
+		private static bool btnRPlus;
+		private static bool btnRHome;
+		private static bool btnRR;
+		private static bool btnRZR;
+
+		#endregion
+
+		#endregion
+
+		#region Flow
 
 		public static void Main()
 		{
-#pragma warning disable 4014
 			MainAsync();
-#pragma warning restore 4014
 		}
 
 		private static async Task MainAsync()
@@ -56,62 +108,28 @@ namespace JoyconVR
 			Console.WriteLine("left.bat");
 			Console.WriteLine("right.bat");
 
+
 			await ScanAsync();
+
+			foreach (var j in _joyconManager.j)
+			{
+				j.SetFilterCoeff(1);
+			}
+
+
 			await LoopAsync(TimeSpan.MinValue, Cancel);
 		}
-
 
 		private static async Task LoopAsync(TimeSpan interval, CancellationToken cancellationToken)
 		{
 			while (true)
 			{
 				await UpdateAsync();
+				await ProcessButtonsAsync();
 				await Task.Delay(interval, cancellationToken);
 			}
 
 			// ReSharper disable once FunctionNeverReturns
-		}
-
-		private static async Task UpdateAsync()
-		{
-			_joyconManager.Update();
-
-			await UpdateInfoAsync();
-			await UpdatePositionDataAsync();
-			await UpdateDeviceDataAsync();
-		}
-
-
-		private static async Task UpdatePositionDataAsync()
-		{
-			_lPos = _lPos + Rotate3D(_lAcc, _lRot);
-			_rPos = _rPos + Rotate3D(_rAcc, _rRot);
-			await EmptyAsync();
-		}
-
-		private static Vector3 Rotate3D(Vector3 point, Vector3 rotation)
-		{
-			var temp = new Math3D.Math3D.Vector3D(point.X, point.Y, point.Z);
-			Math3D.Math3D.RotateX(temp, (float) rotation.X);
-			Math3D.Math3D.RotateY(temp, (float) rotation.Y);
-			Math3D.Math3D.RotateZ(temp, (float) rotation.Z);
-			return new Vector3(temp.x, temp.y, temp.z);
-		}
-
-
-		private static async Task UpdateDeviceDataAsync()
-		{
-			Console.WriteLine("client_commandline.exe setdeviceposition 0 " + VectorToString(_lPos));
-			Console.WriteLine("client_commandline.exe setdeviceposition 1 " + VectorToString(_rPos));
-			Console.WriteLine("client_commandline.exe setdevicerotation 0 " + VectorToString(_lRot));
-			Console.WriteLine("client_commandline.exe setdevicerotation 1 " + VectorToString(_rRot));
-			await EmptyAsync();
-		}
-
-
-		private static string VectorToString(Vector3 v)
-		{
-			return v.X + " " + v.Y + " " + v.Z;
 		}
 
 		private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
@@ -120,20 +138,24 @@ namespace JoyconVR
 			_joyconManager.OnApplicationQuit();
 		}
 
-		private static async Task ScanAsync()
-		{
-			_joyconManager.Scan();
+		#endregion
 
-			UpdateDebugType();
+		#region DataHandling
+
+		private static async Task UpdateAsync()
+		{
+			_joyconManager.Update();
 			await UpdateInfoAsync();
+			await UpdatePositionDataAsync();
+			await UpdateDeviceDataAsync();
 		}
 
-		private static void UpdateDebugType()
+		private static async Task UpdatePositionDataAsync()
 		{
-			foreach (var j in _joyconManager.j)
-				j.debug_type = Joycon.DebugType.NONE;
+			_lPos = _lPos + Rotate3D(_lAcc, _lRot);
+			_rPos = _rPos + Rotate3D(_rAcc, _rRot);
+			await EmptyAsync();
 		}
-
 
 		private static async Task UpdateInfoAsync()
 		{
@@ -166,10 +188,154 @@ namespace JoyconVR
 			await EmptyAsync();
 		}
 
+		private static async Task UpdateDeviceDataAsync()
+		{
+			Console.WriteLine("client_commandline.exe setdeviceposition 0 " + VectorToString(_lPos));
+			Console.WriteLine("client_commandline.exe setdeviceposition 1 " + VectorToString(_rPos));
+			Console.WriteLine("client_commandline.exe setdevicerotation 0 " + VectorToString(_lRot));
+			Console.WriteLine("client_commandline.exe setdevicerotation 1 " + VectorToString(_rRot));
+			await EmptyAsync();
+		}
+
+		private static async Task ProcessButtonsAsync()
+		{
+			Joycon leftJoycon = null;
+			Joycon rightJoycon = null;
+
+			foreach (var joycon in _joyconManager.j)
+			{
+				if (joycon.isLeft)
+					leftJoycon = joycon;
+				else
+					rightJoycon = joycon;
+			}
+
+			#region LeftButtons
+
+			if (leftJoycon != null)
+			{
+				if (leftJoycon.GetButton(Joycon.Button.MINUS))
+					rightJoycon.Recenter();
+
+				if (leftJoycon.GetButton(Joycon.Button.SL))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.SR))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.CAPTURE))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.STICK))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.DPAD_UP))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.DPAD_DOWN))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.DPAD_LEFT))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.DPAD_RIGHT))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.SHOULDER_1))
+					;
+
+				if (leftJoycon.GetButton(Joycon.Button.SHOULDER_2))
+					;
+			}
+
+			#endregion
+
+
+			#region RightButtons
+
+			if (rightJoycon != null)
+			{
+				if (rightJoycon.GetButton(Joycon.Button.MINUS))
+					rightJoycon.Recenter();
+
+				if (rightJoycon.GetButton(Joycon.Button.SL))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.SR))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.CAPTURE))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.STICK))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.DPAD_UP))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.DPAD_DOWN))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.DPAD_LEFT))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.DPAD_RIGHT))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.SHOULDER_1))
+					;
+
+				if (rightJoycon.GetButton(Joycon.Button.SHOULDER_2))
+					;
+			}
+
+			#endregion
+
+			await EmptyAsync();
+		}
+
+		#endregion
+
+		#region JoyConStuff
+
+		private static async Task ScanAsync()
+		{
+			_joyconManager.Scan();
+
+			UpdateDebugType();
+			await UpdateInfoAsync();
+		}
+
+		private static void UpdateDebugType()
+		{
+			foreach (var j in _joyconManager.j)
+				j.debug_type = Joycon.DebugType.NONE;
+		}
+
+		#endregion
+
+		#region Utilities
+
+		private static Vector3 Rotate3D(Vector3 point, Vector3 rotation)
+		{
+			var temp = new Math3D.Math3D.Vector3D(point.X, point.Y, point.Z);
+			Math3D.Math3D.RotateX(temp, (float) rotation.X);
+			Math3D.Math3D.RotateY(temp, (float) rotation.Y);
+			Math3D.Math3D.RotateZ(temp, (float) rotation.Z);
+			return new Vector3(temp.x, temp.y, temp.z);
+		}
+
+		private static string VectorToString(Vector3 v)
+		{
+			return v.X + " " + v.Y + " " + v.Z;
+		}
+
 #pragma warning disable 1998
 		private static async Task EmptyAsync()
 		{
 		}
 #pragma warning restore 1998
+
+		#endregion
 	}
 }
