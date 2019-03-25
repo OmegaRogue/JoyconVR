25e607083507695389faedce6113677efc116939
OmegaRogue <philipp@buennagel.com>
2019-03-25 17:04:02 +0100

    Started Button Proccessing
    Signed-off-by: OmegaRogue <philipp@buennagel.com>

--- JoyconVR/Program.cs
+++ JoyconVR/Program.cs
@@ -218,34 +218,34 @@ namespace JoyconVR
 					rightJoycon.Recenter();
 
 				if (leftJoycon.GetButton(Joycon.Button.SL))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.SR))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.CAPTURE))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.STICK))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.DPAD_UP))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.DPAD_DOWN))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.DPAD_LEFT))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.DPAD_RIGHT))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.SHOULDER_1))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (leftJoycon.GetButton(Joycon.Button.SHOULDER_2))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 			}
 
 			#endregion
@@ -259,34 +259,34 @@ namespace JoyconVR
 					rightJoycon.Recenter();
 
 				if (rightJoycon.GetButton(Joycon.Button.SL))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.SR))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.CAPTURE))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.STICK))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.DPAD_UP))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.DPAD_DOWN))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.DPAD_LEFT))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.DPAD_RIGHT))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.SHOULDER_1))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 
 				if (rightJoycon.GetButton(Joycon.Button.SHOULDER_2))
-					;
+					Console.WriteLine("client_commandline.exe press 0 ");
 			}
 
 			#endregion
